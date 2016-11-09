fa307346c2f4e979b6c1e6758fb9b108c2b48d36
Parth Gaglani <gaglani@ca.ibm.com>
2016-11-09 15:32:17 -0500

    push

---
+++ src/main/java/projecta/com/ibm/projecta/App.java
@@ -0,0 +1,14 @@
+package projecta.com.ibm.projecta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String talk() {
+    	return "ProjectA";
+    }
+    
+   
+}
---
+++ src/test/java/projecta/com/ibm/projecta/TestOutput.java
@@ -0,0 +1,11 @@
+package projecta.com.ibm.projecta;
+
+public class TestOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(App.talk());
+
+	}
+
+}
